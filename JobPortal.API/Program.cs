d243e724cf2363dac1c0a7fb1b0d356bcb82e9ea
Lum Berisha <lumberiisha@gmail.com>
2024-07-05 13:16:11 +0200

    UnitOfWork

--- JobPortal.API/Program.cs
+++ JobPortal.API/Program.cs
@@ -1,12 +1,26 @@
 
+using JobPortal.Application.Interfaces;
+using JobPortal.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.Loader;
+
+
 namespace JobPortal.API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
 
+            var files = Directory.GetFiles(
+                    AppDomain.CurrentDomain.BaseDirectory,
+                    "JobPortal*.dll");
+
+            var assemblies = files
+                .Select(p => AssemblyLoadContext.Default.LoadFromAssemblyPath(p));
+
+
+            var builder = WebApplication.CreateBuilder(args);
             // Add services to the container.
 
             builder.Services.AddControllers();
@@ -14,6 +28,16 @@ namespace JobPortal.API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
+            builder.Services.AddAdvancedDependencyInjection();
+
+            builder.Services.Scan(p => p.FromAssemblies(assemblies)
+                .AddClasses()
+                .AsMatchingInterface());
+
+            builder.Services.AddDbContext<DatabaseService>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
---
+++ JobPortal.Persistence/DatabaseService.cs
@@ -0,0 +1,38 @@
+﻿using JobPortal.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace JobPortal.Persistence
+{
+    public class DatabaseService: DbContext, IDatabaseService
+    {
+        private readonly IConfiguration _configuration;
+
+        public DatabaseService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            Database.EnsureCreated();
+        }
+        //DbSet
+       
+        public void Save()
+        {
+            this.SaveChanges();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            
+        }
+    }
+}
