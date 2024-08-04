649ca71ff794a5e462d4d78ac438f560ae19b720
lumberiisha <96344590+lumberiisha@users.noreply.github.com>
2024-08-04 23:12:35 +0200

    Update ElasticSearchConfiguration.cs

--- JobPortal.Infrastructure/Configurations/ElasticSearchConfiguration.cs
+++ JobPortal.Infrastructure/Configurations/ElasticSearchConfiguration.cs
@@ -2,7 +2,7 @@
 {
     public static class ElasticSearchConfiguration
     {
-        public static string ConnectionString = "34.141.119.232:9200";
+        public static string ConnectionString = "http://34.141.119.232:9200";
         public static string DefaultIndex = "the_index";
         public static string SH256Fingerprint = "70:C5:B5:B0:3B:9A:91:1F:16:AC:48:1F:6F:7C:35:BD:DF:5E:F4:B2:44:4E:C0:3E:CB:52:A9:65:C3:C0:5D:4B";
         public static string User = "elastic";
